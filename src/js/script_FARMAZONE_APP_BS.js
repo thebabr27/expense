d160b782a0d6d13ddade3bd8ce39e114b6f34485
thebabr27 <47173624+thebabr27@users.noreply.github.com>
2019-06-14 22:14:51 +0200

    Add files via upload

--- src/js/script_FARMAZONE_APP_BS.js
+++ src/js/script_FARMAZONE_APP_BS.js
@@ -1,23 +1,40 @@
+var inputProduct;
+
 function restart() {
-    window.location.href = "index.html";
+    window.location.href = "index_FARMAZONE_APP_BS.html";
 }
 
-function alertDatiFarm (inputFarm) {
+function sceltaFarmacia (inputFarm) {
   localStorage.farmacia = inputFarm.value;
-
   window.location.href = "checkout_FARMAZONE_APP_BS.html";
 
 }
 
-function alertDatiInd (inputNomin, inputInd, inputCit, inputCap) {
-  localStorage.nominativo = inputNomin.value;
-  localStorage.indirizzo = inputInd.value;
-  localStorage.citta = inputCit.value;
-  localStorage.cap = inputCap.value;
+function datiCliente (inputName, inputSurname, inputEmail, inputInd, inputCap, inputCit, inputPhone) {
+  localStorage.nominativo = inputName.value + " " + inputSurname.value;
+  localStorage.indirizzo = inputInd.value + " " + inputCap.value + " " + inputCit.value;
   window.location.href = "selectpharm_FARMAZONE_APP_BS.html";
 }
 
-function alertDatiProd (inputProduct) {
+function cercaFarmaci(a) {
+  localStorage.cercato = a.value;
+  window.location.href = "product_FARMAZONE_APP_BS.html";
+}
+
+function elencoFarmaci (inputProduct) {
   localStorage.prodotto = inputProduct.value;
+  window.location.href = "product_FARMAZONE_APP_BS.html";
+}
+
+var carrello = [];
+
+var thisdiv;
+function selezioneFarmaco (thisdiv, b) {
+  carrello.push(b);
+
+};
+
+function sceltaFarmaco () {
+  localStorage.carrello = carrello;
   window.location.href = "address_FARMAZONE_APP_BS.html";
 }
--- src/js/script_checkout_FARMAZONE_APP_BS.js
+++ src/js/script_checkout_FARMAZONE_APP_BS.js
@@ -1,11 +1,42 @@
-document.getElementById("customerData").innerHTML =
-localStorage.nominativo + ", " + localStorage.indirizzo + ", " +
-localStorage.citta; + ", " + localStorage.cap;
+var items = [
+  ["Tachipirina 1000", "Bustine 20g", 5.49],
+  ["Okitask", "Bustine 10g", 4.5],
+  ["Vivin C", "Compresse", 6.69],
+  ["Aspirina C", "Compresse", 5.99],
+]
 
-document.getElementById("item1Name").innerHTML = localStorage.prodotto;
-document.getElementById("item1Amount").innerHTML = "€"+"5.00";
-document.getElementById("rideAmount").innerHTML = "€"+"1.00";
-document.getElementById("totalAmount").innerHTML = "€"+"6.00";
+alert(localStorage.carrello);
+createElenco();
+
+function createElenco() {
+  var elenco = "";
+  var somma = 0;
+  for (var i = 0; i < items.length; i++) {
+    //aggiunta dei prodotti
+    elenco+='<li class="list-group-item d-flex justify-content-between lh-condensed">\r'+
+      '<div>\r'+
+        '<h6 class="my-0">'+items[i][0]+'</h6>\r'+
+        '<small class="text-muted">'+items[i][1]+'</small>\r'+
+      '</div>\r'+
+      '<span class="text-muted">€'+(items[i][2]).toFixed(2)+'</span>\r'+
+    '</li>\r'
+    somma+=(items[i][2]);
+  }
+  //aggiunta del totale
+  elenco+='<li class="list-group-item d-flex justify-content-between lh-condensed">\r'+
+    '<span>Totale</span>\r'+
+    '<strong>€'+somma+'</strong>\r'+
+  '</li>\r'
+  document.getElementById("itemsList").innerHTML = elenco;
+  document.getElementById("itemsNum").innerHTML = items.length;
+}
+
+document.getElementById("customerData").innerHTML =
+localStorage.nominativo + "<br>" +
+localStorage.indirizzo;
+
+document.getElementById("pharmacieData").innerHTML =
+localStorage.farmacia;
 
 function done () {
 
