db789788d507ca8cfceb9bb1a287a96ac1efe04a
thebabr27 <47173624+thebabr27@users.noreply.github.com>
2019-06-15 01:41:04 +0200

    Add files via upload

--- src/js/script_checkout_FARMAZONE_APP_BS.js
+++ src/js/script_checkout_FARMAZONE_APP_BS.js
@@ -1,9 +1,19 @@
 var items = [
-  ["Tachipirina 1000", "Bustine 20g", 5.49],
-  ["Okitask", "Bustine 10g", 4.5],
-  ["Vivin C", "Compresse", 6.69],
-  ["Aspirina C", "Compresse", 5.99],
-]
+  ["Tachipirina 1000", "Bustine 20g", 5.49, 'tachipirina1000',
+  '1000mg compresse ',
+  'Trattamento sintomatico delle affezioni dolorose di ogni genere (ad esempio, mal di testa, mal di denti, torcicollo, dolori articolari e lombosacrali, dolori mestruali, piccoli interventi chirurgici).'],
+  ["Okitask", "40mg 10 compresse", 4.69, 'oki1',
+  'Ketoprofene Sale Di Lisina',
+  'Dolori di diversa origine e natura, ed in particolare: mal di testa, mal di denti, nevralgie, dolori mestruali, dolori muscolari e osteoarticolari.'],
+  ["Vivin C", "530mg 20 compresse effervescenti", 5.99, 'vivin-c',
+  'Acido acetilsalicilico 0,330g, acido ascorbico 0,200g.',
+  'Mal di testa e di denti, nevralgie, dolori mestruali, dolori reumatici e muscolari. Terapia sintomatica degli stati febbrili e delle sindrom i influenzali e da raffreddamento.'],
+  ["Aspirina C", "500mg 10 compresse", 4.73, 'aspirina1',
+  'Acido acetilsalicilico e derivati.',
+  'Trattamento sintomatico di mal di testa e di dent i, nevralgie, dolori mestruali, dolori reumatici e muscolari.'],
+  ["Tipo di consegna scelta", "Express", 3.50, '',
+  '',''],
+  ]
 
 createElenco();
 
@@ -24,7 +34,7 @@ function createElenco() {
   //aggiunta del totale
   elenco+='<li class="list-group-item d-flex justify-content-between lh-condensed">\r'+
     '<span>Totale</span>\r'+
-    '<strong>€'+somma+'</strong>\r'+
+    '<strong>€'+somma.toFixed(2)+'</strong>\r'+
   '</li>\r'
   document.getElementById("itemsList").innerHTML = elenco;
   document.getElementById("itemsNum").innerHTML = items.length;
